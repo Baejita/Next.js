0fb35711a7616259e06a9a45f86ba13315ab030b
Baejita <homokoro@gmail.com>
2024-06-26 20:18:47 +0700

    layout

--- 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
@@ -3,7 +3,6 @@ import Navigation from "../components/Navigation";
 export default function page() {
   return (
     <div>
-      <Navigation />
       <h1>Cabins page</h1>
     </div>
   );
--- 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/page.js
+++ 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/page.js
@@ -4,7 +4,6 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise</h1>
 
       <Link href="/cabins">Exploe luxury cabins</Link>
