0e5dd5e0ecd7db0bc37afc95a5aa9f654ed226fb
Baejita <homokoro@gmail.com>
2024-06-27 16:01:25 +0700

    555

--- 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
@@ -1,9 +1,16 @@
-import Navigation from "../components/Navigation";
+export default async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
 
-export default function page() {
+  console.log(data);
   return (
     <div>
       <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
