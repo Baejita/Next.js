3bf4a0c2fad5d72bc7e59f3109ba30ac1f1911da
Baejita <homokoro@gmail.com>
2024-06-28 12:43:54 +0700

    useClient

--- 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
@@ -1,8 +1,9 @@
+import Counter from "../components/Counter";
+
 export default async function page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
-  console.log(data);
   return (
     <div>
       <h1>Cabins page</h1>
@@ -11,6 +12,7 @@ export default async function page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter users={data} />
     </div>
   );
 }
