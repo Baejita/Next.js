8cdd85026643c40307c3c1360951a0ff4ba93cbf
Baejita <homokoro@gmail.com>
2024-06-29 17:28:25 +0700

    tailwilds

--- 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/starter/the-wildj-oasis-website/app/cabins/page.js
@@ -1,18 +1,9 @@
-import Counter from "../Counter";
+import Counter from "@/app/_components/Counter";
 
-export default async function page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
+export default function page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
