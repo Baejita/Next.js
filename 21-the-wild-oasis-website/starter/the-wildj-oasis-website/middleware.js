24ebdc66de8e0b170ce370e5ccc3047c95a7e720
Baejita <homokoro@gmail.com>
2024-07-22 19:46:33 +0700

    Finish Login

---
+++ 21-the-wild-oasis-website/starter/the-wildj-oasis-website/middleware.js
@@ -0,0 +1,19 @@
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//   console.log(request);
+
+//   return NextResponse.redirect(new URL("/about", request.url));
+// }
+
+import { auth } from "./app/_library/auth";
+
+export const middleware = auth;
+
+auth;
+
+//บังคัับให้ /account และ /cabin -->มาตรงหน้า /about
+//ปกป้องรูทนี้ ไว้ แล้วพาไปส่วนของ Sign in
+export const config = {
+  matcher: ["/account"],
+};
