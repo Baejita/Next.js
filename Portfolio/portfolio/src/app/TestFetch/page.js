165ce73f86a89e7e00047c9192f0f16c077cf922
Baejita <homokoro@gmail.com>
2024-08-02 23:37:37 +0700

    test

--- Portfolio/portfolio/src/app/TestFetch/page.js
+++ Portfolio/portfolio/src/app/TestFetch/page.js
@@ -1,22 +1,7 @@
-"use client";
-import { useEffect, useState } from "react";
-import { supabase } from "../services/supabase";
-
 function page() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    supabase
-      .from("Test")
-      .select("*")
-      .then(({ data }) => setData(data));
-  }, []);
   return (
     <div>
-      <h1>Hi </h1>
-      {data.map((item) => {
-        return <p key={item.id}>{item.fullName}</p>;
-      })}
+      <h1>Test</h1>
     </div>
   );
 }
