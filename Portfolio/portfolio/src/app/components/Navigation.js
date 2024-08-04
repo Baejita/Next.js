010600cbf06a4e195f33b4d61943e8ee6c08139b
Baejita <homokoro@gmail.com>
2024-08-04 14:39:33 +0700

    contactUs

--- Portfolio/portfolio/src/app/components/Navigation.js
+++ Portfolio/portfolio/src/app/components/Navigation.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 export default function Navigation() {
   const path = usePathname();
+
   return (
     <nav className="z-10 text-xl text-primary-500 border-b-2">
       <ul className="flex gap-10 items-center ">
